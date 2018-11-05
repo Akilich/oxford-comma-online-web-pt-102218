0ee541612aea0779364cfcd835a724f548ddb5f3
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 07:06:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-array.length == 1 
+if array.length == 1 
   array.join('')
   
-array.length == 2 
+elsif array.length == 2 
   array.join(' and ')
   
-array.length >= 3 
+elsif array.length >= 3 
     array[-1] = "and #{array[-1]}"
     array.join(', ')
 end
